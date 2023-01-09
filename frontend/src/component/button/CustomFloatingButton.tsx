ddc08ac1d7be2456ece13b8e3144bd9306abac8f
surasit <surasit@digio.co.th>
2023-01-10 01:29:07 +0700

    fix fade floating button

--- frontend/src/component/button/CustomFloatingButton.tsx
+++ frontend/src/component/button/CustomFloatingButton.tsx
@@ -1,5 +1,5 @@
 
-import { Fab } from "@mui/material";
+import { Fab, Fade } from "@mui/material";
 import { useEffect, useState } from "react";
 import themes from "../../constant/themes";
 import CustomIcon from "../icon/CustomIcon";
@@ -25,20 +25,21 @@ export default function CustomFloatingButton() {
     };
 
     return (
-        <Fab
-            color="primary"
-            aria-label="scroll to top"
-            sx={{
-                position: "fixed",
-                display: scrollPosition > 0 ? "" : "none",
-                bottom: { xs: `${themes.marginComponent / 2}px`, md: `${themes.marginComponent}px` },
-                right: { xs: `${themes.marginComponent / 2}px`, md: `${themes.marginComponent}px` },
-                maxWidth: { xs: `${sizeButton - 4}px`, md: `${sizeButton}px` },
-                maxHeight: { xs: `${sizeButton - 4}px`, md: `${sizeButton}px` },
-            }}
-            onClick={scrollToTop}
-        >
-            <CustomIcon name={"up"} sizeNumber={{ xs: `${(sizeButton - 5) / 2}px`, md: `${sizeButton / 2}px` }} color={"white"} />
-        </Fab>
+        <Fade in={scrollPosition > 0} timeout={{ enter: 500, exit: 500 }}>
+            <Fab
+                color="primary"
+                aria-label="scroll to top"
+                sx={{
+                    position: "fixed",
+                    bottom: `${themes.marginComponent}px`,
+                    right: `${themes.marginComponent}px`,
+                    width: `${sizeButton}px`,
+                    height: `${sizeButton}px`,
+                }}
+                onClick={scrollToTop}
+            >
+                <CustomIcon name={"up"} sizeNumber={`${sizeButton / 2}px`} color={"white"} />
+            </Fab>
+        </Fade>
     );
 }
